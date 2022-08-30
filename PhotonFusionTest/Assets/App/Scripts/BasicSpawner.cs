38d149e65bdf7d98addf8c50419b4f503ddbf12b
SeymurAnsari <72018552+SeymurAnsari@users.noreply.github.com>
2022-08-30 18:27:20 +0400

    Added ball with fusion physx

--- PhotonFusionTest/Assets/App/Scripts/BasicSpawner.cs
+++ PhotonFusionTest/Assets/App/Scripts/BasicSpawner.cs
@@ -13,10 +13,13 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 	private NetworkRunner _runner;
 
 	private bool _mouseButton0;
+	private bool _mouseButton1;
 
 	private void Update ()
 	{
 		_mouseButton0 = _mouseButton0 | Input.GetMouseButton (0);
+		_mouseButton1 = _mouseButton1 || Input.GetMouseButton(1);
+
 	}
 
 	public void OnPlayerJoined (NetworkRunner runner, PlayerRef player)
@@ -60,6 +63,10 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 		if (_mouseButton0)
 			data.buttons |= NetworkInputData.MOUSEBUTTON1;
 		_mouseButton0 = false;
+		
+		if (_mouseButton1)
+			data.buttons |= NetworkInputData.MOUSEBUTTON2;
+		_mouseButton1 = false;
 		
 		input.Set (data);
 	}
--- PhotonFusionTest/Assets/App/Scripts/Player.cs
+++ PhotonFusionTest/Assets/App/Scripts/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 public class Player : NetworkBehaviour
 {
 	[SerializeField] private Ball ballPrefab;
+	[SerializeField] private PhysxBall physxBallPrefab;
 	[Networked] private TickTimer delay { get; set; }
-	
+
 	private Vector3 forwardVector;
 
 
@@ -33,12 +34,21 @@ public class Player : NetworkBehaviour
 			{
 				if ((data.buttons & NetworkInputData.MOUSEBUTTON1) != 0)
 				{
-					delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
-					Runner.Spawn (ballPrefab, transform.position + forwardVector, Quaternion.LookRotation (forwardVector), Object.InputAuthority,(runner, obj) =>
+					delay = TickTimer.CreateFromSeconds (Runner, 0.5f);
+					Runner.Spawn (ballPrefab, transform.position + forwardVector, Quaternion.LookRotation (forwardVector), Object.InputAuthority, (runner, obj) =>
 					{
 						// Initialize the Ball before synchronizing it
-						obj.GetComponent<Ball>().Init();
+						obj.GetComponent<Ball> ().Init ();
 					});
+				}
+				else if ((data.buttons & NetworkInputData.MOUSEBUTTON2) != 0)
+				{
+					delay = TickTimer.CreateFromSeconds (Runner, 0.5f);
+					Runner.Spawn (physxBallPrefab,
+						transform.position + forwardVector,
+						Quaternion.LookRotation (forwardVector),
+						Object.InputAuthority,
+						(runner, obj) => { obj.GetComponent<PhysxBall> ().Init (10 * forwardVector); });
 				}
 			}
 		}
